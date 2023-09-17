12677da62ed2e54e14907870784f0ca29b0c3f7f
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2023-09-17 17:43:03 +0200

    New Igeneric and Generic class and Interface added

--- EcoPower_Logistics (1)/SuperStore P3/SuperStore P3/Repository/GenericRepository.cs
+++ EcoPower_Logistics (1)/SuperStore P3/SuperStore P3/Repository/GenericRepository.cs
@@ -1,6 +1,45 @@
-﻿namespace EcoPower_Logistics.Repository
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace EcoPower_Logistics.Repository
 {
-    public class GenericRepository
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- EcoPower_Logistics (1)/SuperStore P3/SuperStore P3/Repository/IGenericRepository.cs
+++ EcoPower_Logistics (1)/SuperStore P3/SuperStore P3/Repository/IGenericRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace EcoPower_Logistics.Repository
+﻿using System.Collections;
+using System.Linq.Expressions;
+
+namespace EcoPower_Logistics.Repository
 {
-    public interface IGenericRepository
+    public interface IGenericRepository<T> where T : class
     {
+        T GetByID(int id);
+        IEnumerable<T> GetAll();
+
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
     }
 }
