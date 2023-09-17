141c672ed66280405f221fbb6171c6e2c0057880
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2023-09-17 18:02:32 +0200

    Added new code in my generic class

--- EcoPower_Logistics (1)/SuperStore P3/SuperStore P3/Repository/GenericRepository.cs
+++ EcoPower_Logistics (1)/SuperStore P3/SuperStore P3/Repository/GenericRepository.cs
@@ -7,39 +7,46 @@ namespace EcoPower_Logistics.Repository
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
+        protected readonly SuperStoreContext _context;
+
+        public GenericRepository(SuperStoreContext context) 
+        {
+            _context = context;
+        }
+
         public void Add(T entity)
         {
-            throw new NotImplementedException();
+            _context.Set<T>().Add(entity);
         }
 
         public void AddRange(IEnumerable<T> entities)
         {
-            throw new NotImplementedException();
+            _context.Set<T>().AddRange(entities);
         }
 
-        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
         {
-            throw new NotImplementedException();
+            return _context.Set<T>().Where(expression);
         }
 
         public IEnumerable<T> GetAll()
         {
-            throw new NotImplementedException();
+            return _context.Set<T>().ToList();
         }
 
         public T GetByID(int id)
         {
-            throw new NotImplementedException();
+            return _context.Set<T>().Find(id);
         }
 
         public void Remove(T entity)
         {
-            throw new NotImplementedException();
+            _context.Set<T>().Remove(entity);
         }
 
         public void RemoveRange(IEnumerable<T> entities)
         {
-            throw new NotImplementedException();
+            _context.Set<T>().RemoveRange(entities);
         }
     }
 }
