3fd9904827eb6738416282ddaca310dc7d651a2d
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2023-09-17 13:34:25 +0200

    Added new Classes under repository file

--- EcoPower_Logistics (1)/SuperStore P3/SuperStore P3/Repository/ProductRepository.cs
+++ EcoPower_Logistics (1)/SuperStore P3/SuperStore P3/Repository/ProductRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace EcoPower_Logistics.Repository
+﻿using Data;
+using Models;
+using System.Collections;
+
+namespace EcoPower_Logistics.Repository
 {
     public class ProductRepository
     {
+        protected readonly SuperStoreContext _storeContext = new SuperStoreContext();
+
+        public async IEnumerable<Product> GetAll()
+        { 
+            return _storeContext.Products.ToList();
+        }
     }
 }
+ 
